64d84a508a2434cdf3bdf876bd2fa30f0cc86bb3
akashbade007 <akashbade007@gmail.com>
2024-08-21 15:06:41 +0530

    changes in test

--- src/test/java/api/test/DDTests.java
+++ src/test/java/api/test/DDTests.java
@@ -21,9 +21,6 @@ public class DDTests {
 
         Response response = UserEndpoints.CreateUser(userPayload);
         Assert.assertEquals(response.getStatusCode(), 200);
-        Assert.assertEquals(response.getStatusCode(), 200);
-        Assert.assertEquals(response.getStatusCode(), 200);
-        Assert.assertEquals(response.getStatusCode(), 200);
         
     }
 
